92c4dd070562299e5aa6d9c4969de128db2b4741
Eugene Obrezkov <ghaiklor@gmail.com>
2015-12-12 16:20:03 +0200

    refactor(shape): Move fill method from Cursor to Shape

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -7,6 +7,23 @@ import Shape from 'kittik-shape-basic';
  * @version 1.0.0
  */
 export default class Rectangle extends Shape {
+  fill(cursor, options = {}) {
+    let {x, y, width, height, symbol = ' ', background, foreground} = options;
+    let filler = symbol.repeat(width + 1);
+
+    if (typeof background !== 'undefined') cursor.background(background);
+    if (typeof foreground !== 'undefined') cursor.foreground(foreground);
+
+    cursor.moveTo(x, y);
+
+    while (y <= height + y) {
+      cursor.write(filler);
+      cursor.moveTo(x, ++y);
+    }
+
+    return this;
+  }
+  
   render(cursor) {
     let text = this.getText();
     let width = this.getWidth();
@@ -16,7 +33,7 @@ export default class Rectangle extends Shape {
     let background = this.getBackground();
     let foreground = this.getForeground();
 
-    cursor.fill({x1, y1, x2, y2, background, foreground});
+    this.fill(cursor, {x1, y1, x2, y2, background, foreground});
     cursor.setPosition(x1 + (width / 2 - text.length / 2), y1 + (height / 2)).write(text);
 
     return this;
