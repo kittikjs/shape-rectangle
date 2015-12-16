5bfde7d200b11fcd12e84ecf31bc876edfa95ec5
ghaiklor <ghaiklor@gmail.com>
2015-12-16 16:23:07 +0200

    test(shape): Add missing tests

--- test/unit/Rectangle.test.js
+++ test/unit/Rectangle.test.js
@@ -9,16 +9,10 @@ describe('Shape::Rectangle', () => {
     let rectangle = new Rectangle();
     let mock = sinon.mock(cursor);
 
-    mock.expects('fill').once().withArgs({
-      x1: 10,
-      y1: 10,
-      x2: 24,
-      y2: 14,
-      background: undefined,
-      foreground: undefined
-    });
-    mock.expects('setPosition').once().withArgs(17.5, 12.5).returns(cursor);
-    mock.expects('write').once().withArgs('');
+    mock.expects('background').never();
+    mock.expects('foreground').never();
+    mock.expects('moveTo').exactly(7).returns(cursor);
+    mock.expects('write').exactly(6).returns(cursor);
 
     rectangle.render(cursor);
 
@@ -38,16 +32,10 @@ describe('Shape::Rectangle', () => {
       foreground: COLORS.BLACK
     });
 
-    mock.expects('fill').once().withArgs({
-      x1: 1,
-      y1: 1,
-      x2: 11,
-      y2: 11,
-      background: 'yellow',
-      foreground: 'black'
-    });
-    mock.expects('setPosition').once().withArgs(4.5, 6.5).returns(cursor);
-    mock.expects('write').once().withArgs('test');
+    mock.expects('background').once().withArgs(11);
+    mock.expects('foreground').once().withArgs(0);
+    mock.expects('moveTo').exactly(13).returns(cursor);
+    mock.expects('write').exactly(12).returns(cursor);
 
     rectangle.render(cursor);
 
@@ -66,6 +54,8 @@ describe('Shape::Rectangle', () => {
         height: 5,
         x: 10,
         y: 10,
+        alignX: 'none',
+        alignY: 'none',
         background: undefined,
         foreground: undefined,
         animation: undefined
@@ -86,7 +76,9 @@ describe('Shape::Rectangle', () => {
         foreground: undefined,
         animation: {
           name: 'print',
-          interval: 100
+          options: {
+            interval: 100
+          }
         }
       }
     };
@@ -96,12 +88,12 @@ describe('Shape::Rectangle', () => {
     assert.equal(rectangle.getText(), 'test');
     assert.equal(rectangle.getWidth(), 30);
     assert.equal(rectangle.getHeight(), 50);
-    assert.deepEqual(rectangle.getPosition(), {x: 0, y: 0});
+    assert.equal(rectangle.getX(), 0);
+    assert.equal(rectangle.getY(), 0);
     assert.isUndefined(rectangle.getBackground());
     assert.isUndefined(rectangle.getForeground());
-    assert.deepEqual(rectangle.getAnimation(), {name: 'print', interval: 100});
-    assert.equal(rectangle.get('animation.name'), 'print');
-    assert.equal(rectangle.get('animation.interval'), 100);
+    assert.equal(rectangle.getAnimationName(), 'print');
+    assert.deepEqual(rectangle.getAnimationOptions(), {interval: 100});
     assert.ok(rectangle.isAnimated());
   });
 });
