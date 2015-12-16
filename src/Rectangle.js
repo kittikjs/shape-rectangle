18dbcfa13ba598b97b3396f90bef0833954eb5bb
ghaiklor <ghaiklor@gmail.com>
2015-12-16 16:33:04 +0200

    fix(shape): Simplify logic where calculates coordinates

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -13,8 +13,8 @@ export default class Rectangle extends Shape {
     let height = this.getHeight();
     let x1 = this.getX();
     let y1 = this.getY();
-    let x2 = x1 + width - 1;
-    let y2 = y1 + height - 1;
+    let x2 = x1 + width;
+    let y2 = y1 + height;
     let background = this.getBackground();
     let foreground = this.getForeground();
     let filler = ' '.repeat(width);
@@ -24,12 +24,12 @@ export default class Rectangle extends Shape {
 
     cursor.moveTo(x1, y1);
 
-    while (y1 <= y2) {
+    for (let y = y1; y <= y2; y++) {
       cursor.write(filler);
-      cursor.moveTo(x1, ++y1);
+      cursor.moveTo(x1, y);
     }
 
-    cursor.moveTo(x1 + (width / 2 - text.length / 2), this.getY() + (height / 2)).write(text);
+    cursor.moveTo(x1 + (width / 2 - text.length / 2), y1 + (height / 2)).write(text);
 
     return this;
   }
