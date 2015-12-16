fb112f3da7ead93ef3e5a8bee9fe0be8c5da4d92
ghaiklor <ghaiklor@gmail.com>
2015-12-16 16:22:53 +0200

    fix(shape): Migrate to new basic shape and cursor

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -7,34 +7,29 @@ import Shape from 'kittik-shape-basic';
  * @version 1.0.0
  */
 export default class Rectangle extends Shape {
-  fill(cursor, options = {}) {
-    let {x, y, width, height, symbol = ' ', background, foreground} = options;
-    let filler = symbol.repeat(width + 1);
-
-    if (typeof background !== 'undefined') cursor.background(background);
-    if (typeof foreground !== 'undefined') cursor.foreground(foreground);
-
-    cursor.moveTo(x, y);
-
-    while (y <= height + y) {
-      cursor.write(filler);
-      cursor.moveTo(x, ++y);
-    }
-
-    return this;
-  }
-  
   render(cursor) {
     let text = this.getText();
     let width = this.getWidth();
     let height = this.getHeight();
-    let {x: x1, y: y1} = this.getPosition();
-    let {x2, y2} = {x2: width + x1 - 1, y2: height + y1 - 1};
+    let x1 = this.getX();
+    let y1 = this.getY();
+    let x2 = x1 + width - 1;
+    let y2 = y1 + height - 1;
     let background = this.getBackground();
     let foreground = this.getForeground();
+    let filler = ' '.repeat(width);
 
-    this.fill(cursor, {x1, y1, x2, y2, background, foreground});
-    cursor.setPosition(x1 + (width / 2 - text.length / 2), y1 + (height / 2)).write(text);
+    if (typeof background !== 'undefined') cursor.background(background);
+    if (typeof foreground !== 'undefined') cursor.foreground(foreground);
+
+    cursor.moveTo(x1, y1);
+
+    while (y1 <= y2) {
+      cursor.write(filler);
+      cursor.moveTo(x1, ++y1);
+    }
+
+    cursor.moveTo(x1 + (width / 2 - text.length / 2), this.getY() + (height / 2)).write(text);
 
     return this;
   }
