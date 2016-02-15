294aafacd298a013881f92f7a866a8b07f5a25f5
ghaiklor <ghaiklor@gmail.com>
2016-02-15 15:45:18 +0200

    fix(shape): Update shape-basic to the latest version

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,22 @@
 const Rectangle = require('../lib/Rectangle');
 const cursor = require('kittik-cursor').create().resetTTY();
 
-Rectangle.create({text: 'Text here!', x: 'center', width: 20, background: 'green', foreground: 'black'}).render(cursor);
-Rectangle.create({x: 'center', y: 'middle', width: '50%', height: '20%', background: 'dark_blue'}).render(cursor);
+Rectangle.create({
+  text: 'Text here!',
+  x: 'center',
+  y: 2,
+  width: 40,
+  background: 'green',
+  foreground: 'black'
+}).render(cursor);
+
+Rectangle.create({
+  text: 'Text here',
+  x: 'center',
+  y: 'middle',
+  width: '50%',
+  height: '20%',
+  background: 'dark_blue'
+}).render(cursor);
 
 cursor.moveTo(1, process.stdout.rows).flush();
