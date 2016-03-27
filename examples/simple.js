d05b32a121c9de3aa2ce58df7ce4c25398864c9d
ghaiklor <ghaiklor@gmail.com>
2016-03-27 13:41:30 +0300

    docs(shape): Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,18 +1,18 @@
 "use strict";
 
 const Rectangle = require('../lib/Rectangle');
-const cursor = require('kittik-cursor').create().resetTTY();
+const cursor = require('kittik-cursor').create().reset();
 
-Rectangle.create({
+Rectangle.create(cursor, {
   text: 'Text here!',
   x: 'center',
   y: 2,
   width: 40,
   background: 'green',
   foreground: 'black'
-}).render(cursor);
+}).render();
 
-Rectangle.create({
+Rectangle.create(cursor, {
   text: 'Text here',
   x: 'center',
   y: 'middle',
@@ -21,4 +21,4 @@ Rectangle.create({
   background: 'dark_blue'
 }).render(cursor);
 
-cursor.moveTo(1, process.stdout.rows).flush();
+cursor.flush();
